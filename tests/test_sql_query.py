15f0e0a1f15531c13e430827b42100572c249beb
Matt Denno <mgdenno@gmail.com>
2024-11-07 12:07:03 -0500

    290 add an sql method to evaluation (#296)
    Adds an SQL query method to the evaluation class.
    Co-authored-by: samlamont <sam.lamont@gmail.com>

---
+++ tests/test_sql_query.py
@@ -0,0 +1,49 @@
+"""This module tests the filter functions on primary_timeseries.
+
+This module tests the filter functions on primary_timeseries. It
+should apply to all tables.
+"""
+import tempfile
+import pytest
+
+from setup_v0_3_study import setup_v0_3_study
+
+
+def test_sql_query(tmpdir):
+    """Test filter string."""
+    ev = setup_v0_3_study(tmpdir)
+    sdf = ev.sql("""
+        SELECT pt.*, u.*, c.* FROM primary_timeseries pt
+        JOIN units u ON pt.unit_name = u.name
+        JOIN configurations c ON pt.configuration_name = c.name
+        LIMIT 10;
+    """)
+    sdf_cols = sdf.columns
+    expected_cols = [
+        'reference_time',
+        'value_time',
+        'value',
+        'unit_name',
+        'location_id',
+        'configuration_name',
+        'variable_name',
+        'name',
+        'long_name',
+        'name',
+        'type',
+        'description'
+    ]
+    assert sdf_cols == expected_cols
+
+
+
+if __name__ == "__main__":
+    with tempfile.TemporaryDirectory(
+        prefix="teehr-"
+    ) as tempdir:
+        test_sql_query(
+            tempfile.mkdtemp(
+                prefix="1-",
+                dir=tempdir
+            )
+        )
