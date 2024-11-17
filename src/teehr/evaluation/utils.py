2e15a5781837c72e93c57ceb3a9054c1e5fde3d6
Matt Denno <mgdenno@gmail.com>
2024-11-17 16:16:57 -0500

    switch to TEEHR print_tree in all notebooks.

--- src/teehr/evaluation/utils.py
+++ src/teehr/evaluation/utils.py
@@ -15,9 +15,23 @@ def get_schema_variable_name(variable_name: str) -> str:
     return NWM_VARIABLE_MAPPER[VARIABLE_NAME]. \
         get(variable_name, variable_name)
 
+PIPE = "│"
+ELBOW = "└──"
+TEE = "├──"
+PIPE_PREFIX = "│   "
+SPACE_PREFIX = "    "
 
-def print_tree(path, prefix="", exclude_patterns: List[str] = [""]):
+def print_tree(
+    path,
+    prefix="",
+    exclude_patterns: List[str] = [""],
+    max_depth: int = -1,
+    current_depth: int = 0
+):
     """Print the directory tree structure."""
+
+    if max_depth != -1 and current_depth > max_depth:
+        return
 
     # Get all files and directories in the current path
     paths = list(Path(path).glob("*"))
@@ -26,10 +40,21 @@ def print_tree(path, prefix="", exclude_patterns: List[str] = [""]):
     filtered_files = [f for f in paths if not any(fnmatch.fnmatch(f.name, p) for p in exclude_patterns)]
 
     # Print the directory tree structure
-    for full_path in filtered_files:
+    entries_count = len(filtered_files)
+
+    for index, full_path in enumerate(filtered_files):
         name = full_path.relative_to(path)
+        connector = ELBOW if index == entries_count - 1 else TEE
+
         if full_path.is_dir():
-            print(f"{prefix}├── {name}")
-            print_tree(path=full_path, prefix=f"{prefix}│   ", exclude_patterns=exclude_patterns)
+            print(f"{prefix}{connector} {name}")
+            new_prefix = prefix + (PIPE_PREFIX if index != entries_count - 1 else SPACE_PREFIX)
+            print_tree(
+                path=full_path,
+                prefix=new_prefix,
+                exclude_patterns=exclude_patterns,
+                max_depth=max_depth,
+                current_depth=current_depth + 1
+            )
         else:
-            print(f"{prefix}├── {name}")
+            print(f"{prefix}{connector} {name}")
