a81fdca07a7b3075ee46ee838c21ae5aef400eac
Matt Denno <mgdenno@gmail.com>
2024-12-01 15:38:59 -0500

    update models, remove non-domain pydantic models, add pandas ans pyspark pandera models

--- src/teehr/models/pydantic_table_models.py
+++ src/teehr/models/pydantic_table_models.py
@@ -52,39 +52,39 @@ class Attribute(TableBaseModel):
     description: str
 
 
-class Location(TableBaseModel):
-    """Location model."""
+# class Location(TableBaseModel):
+#     """Location model."""
 
-    id: str
-    name: str
-    geometry: bytes
-
-
-class LocationAttribute(TableBaseModel):
-    """LocationAttribute model."""
-
-    location_id: str
-    attribute_name: str
-    value: str
+#     id: str
+#     name: str
+#     geometry: bytes
 
 
-class LocationCrosswalk(TableBaseModel):
-    """LocationCrosswalk model."""
+# class LocationAttribute(TableBaseModel):
+#     """LocationAttribute model."""
 
-    primary_location_id: str
-    secondary_location_id: str
+#     location_id: str
+#     attribute_name: str
+#     value: str
 
 
-class Timeseries(TableBaseModel):
-    """Timeseries model."""
+# class LocationCrosswalk(TableBaseModel):
+#     """LocationCrosswalk model."""
 
-    reference_time: datetime
-    value_time: datetime
-    configuration_name: str
-    unit_name: str
-    variable_name: str
-    value: float
-    location_id: str
+#     primary_location_id: str
+#     secondary_location_id: str
+
+
+# class Timeseries(TableBaseModel):
+#     """Timeseries model."""
+
+#     reference_time: datetime
+#     value_time: datetime
+#     configuration_name: str
+#     unit_name: str
+#     variable_name: str
+#     value: float
+#     location_id: str
 
 # Note: there is no JoinedTimeseries model in this file because the
 # JoinedTimeseries model is dynamic and the fields can be different
