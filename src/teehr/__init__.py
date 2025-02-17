320099e9439416158184e8d5d62b1a0d58070d33
samlamont <sam.lamont@gmail.com>
2025-02-17 14:39:26 -0500

    Update changelog and doc updates

--- src/teehr/__init__.py
+++ src/teehr/__init__.py
@@ -44,5 +44,3 @@ from teehr.evaluation.tables.unit_table import UnitTable
 from teehr.evaluation.tables.variable_table import VariableTable
 from teehr.evaluation.fetch import Fetch  # noqa
 from teehr.visualization.dataframe_accessor import TEEHRDataFrameAccessor  # noqa
-from teehr.metrics import deterministic_funcs, signature_funcs, probabilistic_funcs # noqa
-from teehr.metrics.signature_funcs import count  # noqa
