21967cd498a1dab68b7e158e6c441940fb1c5272
samlamont <sam.lamont@gmail.com>
2025-06-18 16:10:04 -0400

    testing with nox

---
+++ noxfile.py
@@ -0,0 +1,8 @@
+"""Nox configuration file for running tests with pytest."""
+import nox_poetry
+
+
+@nox_poetry.session(python=["3.11", "3.12"])
+def tests(session):
+    session.install("lxml", "bokeh", ".")
+    session.run("pytest", "tests/test_add_udfs.py")
