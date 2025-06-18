f195a7719411a1affa5fc448ae113dca04fa6113
samlamont <sam.lamont@gmail.com>
2025-06-18 19:10:29 -0400

    test

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@
 import nox_poetry
 
 
-@nox_poetry.session(python=["3.11", "3.12"])
+@nox_poetry.session()  # python=["3.11", "3.12"]
 def tests(session):
     session.install(".")
     session.run("pytest", "tests/test_add_udfs.py")
