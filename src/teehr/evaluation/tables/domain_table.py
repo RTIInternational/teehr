f8587c11a1906a2288a77268a4070f5572076246
samland1116 <samland1116@gmail.com>
2025-06-23 12:00:12 -0600

    Update domain_table.py _add() method, update clone from s3 test to account for data changes

--- src/teehr/evaluation/tables/domain_table.py
+++ src/teehr/evaluation/tables/domain_table.py
@@ -36,17 +36,27 @@ class DomainTable(BaseTable):
             )
         new_df_validated = self._validate(new_df)
 
-        # warn the user if rows in added data already exist in the table
+        # warn user if rows in added data already exist in the original table
+        df_matched = new_df_validated.join(
+            org_df, on=self.unique_column_set, how="left_semi"
+        )
+        if df_matched.count() == 0:
+            # add the validated new data to the existing data
+            logger.info(f"Adding {len(obj)} objects to {self.name} table")
+            combined_df = org_df.unionByName(new_df_validated).repartition(1)
 
-        # add the validated new data to the existing data
-        logger.info(f"Adding {len(obj)} objects to {self.name} table")
-        combined_df = org_df.unionByName(new_df_validated).repartition(1)
+            # validate the combined data
+            logger.info(
+                f"Validating {self.name} table after adding {len(obj)} objects"
+                )
+            validated_df = self._validate(combined_df)
 
-        # validate the combined data
-        logger.info(
-            f"Validating {self.name} table after adding {len(obj)} objects"
-            )
-        validated_df = self._validate(combined_df)
-
-        # write the validated data to the table
-        self._write_spark_df(validated_df)
+            # write the validated data to the table
+            self._write_spark_df(validated_df)
+        else:
+            matched_count = df_matched.count()
+            logger.warning(
+                f"{matched_count} rows in the added data already exist in the "
+                f"{self.name} table. No data has been added."
+                )
+            df_matched.show(truncate=False)
