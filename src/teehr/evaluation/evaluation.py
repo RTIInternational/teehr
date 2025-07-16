2b0f62e74a85278c4446d7fe583eee85ce20c4ed
samlamont <sam.lamont@gmail.com>
2025-07-16 10:20:19 -0400

    update driver settings based on available memory

--- src/teehr/evaluation/evaluation.py
+++ src/teehr/evaluation/evaluation.py
@@ -1,4 +1,5 @@
 """Evaluation module."""
+import psutil
 from datetime import datetime
 from typing import Union, Literal, List
 from pathlib import Path
@@ -84,6 +85,9 @@ class Evaluation:
         # Create a local Spark Session if one is not provided.
         if not self.spark:
             logger.info("Creating a new Spark session.")
+            memory_info = psutil.virtual_memory()
+            driver_memory = 0.75 * memory_info.available / (1024**3)  # Use 75% of available memory
+            driver_maxresultsize = 0.5 * driver_memory
             conf = (
                 SparkConf()
                 .setAppName("TEEHR")
@@ -96,8 +100,8 @@ class Evaluation:
                 .set("spark.driver.host", "localhost")
                 .set("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.4.1")
                 .set("spark.sql.parquet.enableVectorizedReader", "false")
-                .set("spark.driver.memory", "16g")
-                .set("spark.driver.maxResultSize", "8g")
+                .set("spark.driver.memory", f"{int(driver_memory)}g")
+                .set("spark.driver.maxResultSize", f"{int(driver_maxresultsize)}g")
             )
             self.spark = SparkSession.builder.config(conf=conf).getOrCreate()
 
