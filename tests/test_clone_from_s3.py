10b60f3df51b945c89e1c01a5fffc0a8451890f5
samlamont <sam.lamont@gmail.com>
2025-08-07 13:24:18 -0400

    update clone partial test

--- tests/test_clone_from_s3.py
+++ tests/test_clone_from_s3.py
@@ -47,7 +47,7 @@ def test_clone_partial_template_from_s3(tmpdir):
 
     assert ev.units.to_sdf().count() == 9
     assert ev.variables.to_sdf().count() == 5
-    assert ev.attributes.to_sdf().count() == 8
+    assert ev.attributes.to_sdf().count() == 13
     assert ev.configurations.to_sdf().count() == 1
     assert ev.primary_timeseries.to_sdf().count() == 0
     assert ev.secondary_timeseries.to_sdf().count() == 0
