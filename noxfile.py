f1a786fc016eecfe8b7ed78b74e0cb309cd6700a
samlamont <sam.lamont@gmail.com>
2025-08-08 09:00:53 -0400

    test clone_from_s3

--- noxfile.py
+++ noxfile.py
@@ -3,7 +3,7 @@ import nox_poetry
 
 
 @nox_poetry.session()  # local test, ex: python=["3.11", "3.12"]
-def tests(session):
+def all_tests(session):
     """Run the test suite using pytest."""
     session.install("pytest", ".")  # for coverage report include: "pytest-cov"
     session.run(
@@ -11,4 +11,14 @@ def tests(session):
         # "--cov=teehr",
         # "--ctrf=report.json",
         # "--cov-report=term",
+    )
+
+
+@nox_poetry.session()
+def single_test(session):
+    """Run a single test using pytest."""
+    session.install("pytest", ".")
+    session.run(
+        "pytest",
+        "tests/test_clone_from_s3.py"
     )
