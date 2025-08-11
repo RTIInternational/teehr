4be10b2d8c4819e71a411e3cba635f6ecd9014da
samlamont <sam.lamont@gmail.com>
2025-08-11 09:59:03 -0400

    test dev build

--- noxfile.py
+++ noxfile.py
@@ -2,17 +2,16 @@
 import nox_poetry
 
 
-@nox_poetry.session()  # for local testing, ex: python=["3.12", "3.13"]
-def all_tests(session):
-    """Run the test suite using pytest."""
-    session.install("pytest", ".")  # for coverage report include: "pytest-cov"
-    session.run(
-        "pytest",
-        # "--cov=teehr",
-        # "--ctrf=report.json",
-        # "--cov-report=term",
-    )
-
+# @nox_poetry.session()  # for local testing, ex: python=["3.12", "3.13"]
+# def all_tests(session):
+#     """Run the test suite using pytest."""
+#     session.install("pytest", ".")  # for coverage report include: "pytest-cov"
+#     session.run(
+#         "pytest",
+#         # "--cov=teehr",
+#         # "--ctrf=report.json",
+#         # "--cov-report=term",
+#     )
 
 @nox_poetry.session()  # python=["3.12", "3.13"]
 def single_test(session):
