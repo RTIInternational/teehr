22619b1f4eef203a04e92b57eab330fc92cdc4fd
samlamont <sam.lamont@gmail.com>
2025-09-05 08:11:47 -0400

    0.6.0dev0 version

--- src/teehr/__init__.py
+++ src/teehr/__init__.py
@@ -1,7 +1,7 @@
 """Initialize the TEEHR package."""
 import warnings
 
-__version__ = "0.5.0"
+__version__ = "0.6.0dev0"
 
 with warnings.catch_warnings():
     warnings.simplefilter("ignore", UserWarning)
