e1e6a8e0377729fd634d226718b37af7a5dcd56e
samlamont <sam.lamont@gmail.com>
2025-10-09 15:42:02 -0400

    update initialize warehouse

--- warehouse/initialize_warehouse.py
+++ warehouse/initialize_warehouse.py
@@ -1,177 +1,141 @@
-"""A script for initializing the warehouse directory with necessary schema files."""
+"""A script for initializing the warehouse directory with necessary table
+schemas and loading data into the tables from an existing e4 Evaluation.
+
+Notes
+-----
+This is essentially the same as the convert_to_iceberg utility, but hardcoded
+to write to remote.
+"""
 from pathlib import Path
 
-from teehr.utilities.apply_migrations import evolve_catalog_schema
 from teehr import Evaluation
 
 
-def main():
-    """Run the code to initialize the warehouse schema."""
-    catalog_name = "iceberg"
-    namespace = "e0_2_location_example"  # aka schema, database
-
-    # Load data from an existing local e4 Evaluation
-    e4_evaluation_path = Path("/mnt/c/data/ciroh/teehr/e4_evaluations/e0_2_location_example")
-    # e4_evaluation_path = Path("/mnt/c/data/ciroh/teehr/test_stuff/teehr/warehouse")  # TEMP!
-
+def main(
+    e4_evaluation_path: str | Path = None,
+    remote_catalog_name: str = "iceberg",
+    remote_namespace_name: str = "e0_2_location_example"  # aka schema
+):
+    """Run the code to initialize and load data into the warehouse schema."""
     # All other defaults should be good
     ev = Evaluation(
         local_warehouse_dir=e4_evaluation_path,
-        local_catalog_name="local",
-        local_namespace_name=namespace,
-        remote_catalog_name=catalog_name,
-        remote_namespace_name=namespace,
+        remote_catalog_name=remote_catalog_name,
+        remote_namespace_name=remote_namespace_name,
         create_local_dir=False,
         check_evaluation_version=False,
     )
 
-    # NOTE: This does not go to remote, clone_template only targets local dirs.
-    # If a user wants to apply migrations, run scripts, and
-    # clone data from the remote warehouse, they must set up a template locally.
-    # ev.clone_template(
-    #     catalog_name=catalog_name,
-    #     namespace=namespace
-    # )
-
-    # # Now, for all table-based functions like read/query/write we can pass in
-    # # the catalog, namespace, and table name to operate on:
-    # # - catalog_name
-    # # - namespace_name
-    # # - table_name
-    # remote_sdf = ev.read.from_warehouse(
-    #     catalog_name="iceberg",
-    #     namespace="teehr",
-    #     table="units",
-    #     filters="name = 'mm/s'"
-    # ).to_sdf()
-
-    # local_sdf = ev.read.from_warehouse(
-    #     catalog_name="local",
-    #     namespace="db",
-    #     table="units"
-    # ).to_sdf()
-    # pass
-
-    # Should this also be a component class?
-    # Or use ev.apply_schema_migration()?
-    # NOTE: Migrations could/should be applied separately to different namespaces correct?
-    evolve_catalog_schema(
-        spark=ev.spark,
-        migrations_dir_path=Path(__file__).parent,
-        catalog_name=catalog_name,
-        namespace=namespace
-    )
-
-    # We should be able to read in the tables from the local evaluation.
-    options = {
-        "header": "true",
-        "ignoreMissingFiles": "true"
-    }
-
     ev.set_active_catalog("remote")
 
-    units_table_dir = e4_evaluation_path / "dataset" / "units"
+    # This should create the tables in remote if they don't exist
+    ev.apply_schema_migration()
+
+    # Now copy in the e4 data.
+    options = {
+        "header": "true",
+        "ignoreMissingFiles": "true",
+        "recursiveFileLookup": "true"
+    }
+    dataset_dir = e4_evaluation_path / "dataset"
+
     units_table = ev.units
     schema = units_table.schema_func().to_structtype()
-    units_sdf = ev.spark.read.format(units_table.format).options(**options).load(units_table_dir.as_posix(), schema=schema)
+    units_sdf = ev.spark.read.format("csv").options(**options).load((dataset_dir / "units").as_posix(), schema=schema)
     ev.write.to_warehouse(
         source_data=units_sdf,
         table_name="units",
-        namespace_name=namespace
+        namespace_name=remote_namespace_name
     )
 
-    configuration_table_dir = e4_evaluation_path / "dataset" / "configurations"
     configuration_table = ev.configurations
     schema = configuration_table.schema_func().to_structtype()
-    configuration_sdf = ev.spark.read.format(configuration_table.format).options(**options).load(configuration_table_dir.as_posix(), schema=schema)
+    configuration_sdf = ev.spark.read.format("csv").options(**options).load((dataset_dir / "configurations").as_posix(), schema=schema)
     ev.write.to_warehouse(
         source_data=configuration_sdf,
         table_name="configurations",
-        namespace_name=namespace
+        namespace_name=remote_namespace_name
     )
 
-    variables_table_dir = e4_evaluation_path / "dataset" / "variables"
     variables_table = ev.variables
     schema = variables_table.schema_func().to_structtype()
-    variables_sdf = ev.spark.read.format(variables_table.format).options(**options).load(variables_table_dir.as_posix(), schema=schema)
+    variables_sdf = ev.spark.read.format("csv").options(**options).load((dataset_dir / "variables").as_posix(), schema=schema)
     ev.write.to_warehouse(
         source_data=variables_sdf,
         table_name="variables",
-        namespace_name=namespace
+        namespace_name=remote_namespace_name
     )
 
-    attributes_table_dir = e4_evaluation_path / "dataset" / "attributes"
     attributes_table = ev.attributes
     schema = attributes_table.schema_func().to_structtype()
-    attributes_sdf = ev.spark.read.format(attributes_table.format).options(**options).load(attributes_table_dir.as_posix(), schema=schema)
+    attributes_sdf = ev.spark.read.format("csv").options(**options).load((dataset_dir / "attributes").as_posix(), schema=schema)
     ev.write.to_warehouse(
         source_data=attributes_sdf,
         table_name="attributes",
-        namespace_name=namespace
+        namespace_name=remote_namespace_name
     )
 
-    locations_table_dir = e4_evaluation_path / "dataset" / "locations"
     locations_table = ev.locations
     schema = locations_table.schema_func().to_structtype()
-    locations_sdf = ev.spark.read.format(locations_table.format).options(**options).load(locations_table_dir.as_posix(), schema=schema)
+    locations_sdf = ev.spark.read.format("parquet").options(**options).load((dataset_dir / "locations").as_posix(), schema=schema)
     ev.write.to_warehouse(
         source_data=locations_sdf,
         table_name="locations",
-        namespace_name=namespace
+        namespace_name=remote_namespace_name
     )
 
-    location_attrs_table_dir = e4_evaluation_path / "dataset" / "location_attributes"
     location_attrs_table = ev.location_attributes
     schema = location_attrs_table.schema_func().to_structtype()
-    location_attrs_sdf = ev.spark.read.format(location_attrs_table.format).options(**options).load(location_attrs_table_dir.as_posix(), schema=schema)
+    location_attrs_sdf = ev.spark.read.format("parquet").options(**options).load((dataset_dir / "location_attributes").as_posix(), schema=schema)
     ev.write.to_warehouse(
         source_data=location_attrs_sdf,
         table_name="location_attributes",
-        namespace_name=namespace
+        namespace_name=remote_namespace_name
     )
 
-    primary_timeseries_table_dir = e4_evaluation_path / "dataset" / "primary_timeseries"
     primary_timeseries_table = ev.primary_timeseries
     schema = primary_timeseries_table.schema_func().to_structtype()
-    primary_timeseries_sdf = ev.spark.read.format(primary_timeseries_table.format).options(**options).load(primary_timeseries_table_dir.as_posix(), schema=schema)
+    primary_timeseries_sdf = ev.spark.read.format("parquet").options(**options).load((dataset_dir / "primary_timeseries").as_posix(), schema=schema)
     ev.write.to_warehouse(
         source_data=primary_timeseries_sdf,
         table_name="primary_timeseries",
-        namespace_name=namespace
+        namespace_name=remote_namespace_name
     )
 
-    secondary_timeseries_table_dir = e4_evaluation_path / "dataset" / "secondary_timeseries"
     secondary_timeseries_table = ev.secondary_timeseries
     schema = secondary_timeseries_table.schema_func().to_structtype()
-    secondary_timeseries_sdf = ev.spark.read.format(secondary_timeseries_table.format).options(**options).load(secondary_timeseries_table_dir.as_posix(), schema=schema)
+    secondary_timeseries_sdf = ev.spark.read.format("parquet").options(**options).load((dataset_dir / "secondary_timeseries").as_posix(), schema=schema)
     ev.write.to_warehouse(
         source_data=secondary_timeseries_sdf,
         table_name="secondary_timeseries",
-        namespace_name=namespace
+        namespace_name=remote_namespace_name
     )
 
-    joined_timeseries_table_dir = e4_evaluation_path / "dataset" / "joined_timeseries"
     joined_timeseries_table = ev.joined_timeseries
     schema = joined_timeseries_table.schema_func().to_structtype()
-    joined_timeseries_sdf = ev.spark.read.format(joined_timeseries_table.format).options(**options).load(joined_timeseries_table_dir.as_posix(), schema=schema)
+    joined_timeseries_sdf = ev.spark.read.format("parquet").options(**options).load((dataset_dir / "joined_timeseries").as_posix(), schema=schema)
     ev.write.to_warehouse(
         source_data=joined_timeseries_sdf,
         table_name="joined_timeseries",
-        namespace_name=namespace
+        namespace_name=remote_namespace_name
     )
 
-    location_crosswalk_table_dir = e4_evaluation_path / "dataset" / "location_crosswalks"
     location_crosswalk_table = ev.location_crosswalks
     schema = location_crosswalk_table.schema_func().to_structtype()
-    location_crosswalk_sdf = ev.spark.read.format(location_crosswalk_table.format).options(**options).load(location_crosswalk_table_dir.as_posix(), schema=schema)
+    location_crosswalk_sdf = ev.spark.read.format("parquet").options(**options).load((dataset_dir / "location_crosswalks").as_posix(), schema=schema)
     ev.write.to_warehouse(
         source_data=location_crosswalk_sdf,
         table_name="location_crosswalks",
-        namespace_name=namespace
+        namespace_name=remote_namespace_name
     )
 
     pass
 
 
 if __name__ == "__main__":
-    main()
+    e4_evaluation_path = Path("/mnt/c/data/ciroh/teehr/e4_evaluations/e0_2_location_example")
+    main(
+        e4_evaluation_path=e4_evaluation_path,
+        remote_catalog_name="iceberg",
+        remote_namespace_name="e0_2_location_example"
+    )
