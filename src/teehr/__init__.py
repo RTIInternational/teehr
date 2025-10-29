0288588ec49d0027c736fbc1a9af8ab80f513288
samland1116 <samland1116@gmail.com>
2025-10-29 14:23:47 -0500

    increment dev version in __init__.py and .toml

--- src/teehr/__init__.py
+++ src/teehr/__init__.py
@@ -1,7 +1,7 @@
 """Initialize the TEEHR package."""
 import warnings
 
-__version__ = "0.5.1dev9"
+__version__ = "0.5.1dev10"
 
 with warnings.catch_warnings():
     warnings.simplefilter("ignore", UserWarning)
