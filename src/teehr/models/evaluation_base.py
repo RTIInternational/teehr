5d0e107b7c8ac93ae3d83fb3497d30c041b4d7d1
samlamont <sam.lamont@gmail.com>
2025-11-03 12:27:14 -0500

    update default catalog names in catalog models

--- src/teehr/models/evaluation_base.py
+++ src/teehr/models/evaluation_base.py
@@ -24,45 +24,13 @@ class EvaluationBase:
     # )
 
 
-# class CatalogConfigBase(PydanticBaseModel):
-#     """Base model for catalog configuration."""
-
-#     warehouse_dir: str | Path | None = Field(default=None)
-#     catalog_name: str | None = Field(default=None)
-#     namespace_name: str | None = Field(default=None)
-#     catalog_type: str | None = Field(default=None)
-#     catalog_uri: str | None = Field(default=None)
-#     dataset_dir: str | Path | None = Field(default=None)
-#     cache_dir: str | Path | None = Field(default=None)
-#     scripts_dir: str | Path | None = Field(default=None)
-
-#     model_config = ConfigDict(
-#         arbitrary_types_allowed=True,
-#         validate_assignment=True,
-#         extra='forbid'  # raise an error if extra fields are passed
-#     )
-
-#     @model_validator(mode='before')
-#     @classmethod
-#     def calculate_field(cls, values: Dict[str, Any]) -> Dict[str, Any]:
-#         """Assign dataset, cache, and scripts dirs based on warehouse dir."""
-#         if "warehouse_dir" in values and values["warehouse_dir"] is not None:
-#             values['dataset_dir'] = \
-#                 Path(values["warehouse_dir"]) / const.DATASET_DIR
-#             values['cache_dir'] = \
-#                 Path(values["warehouse_dir"]) / const.CACHE_DIR
-#             values['scripts_dir'] = \
-#                 Path(values["warehouse_dir"]) / const.SCRIPTS_DIR
-#         return values
-
-
 class LocalCatalog(PydanticBaseModel):
     """Base model for local catalog configuration."""
 
     warehouse_dir: str | Path | None = Field(default=None)
-    catalog_name: str | None = Field(default="local")
-    namespace_name: str | None = Field(default="teehr")
-    catalog_type: str | None = Field(default="hadoop")
+    catalog_name: str | None = Field(default=const.LOCAL_CATALOG_NAME)
+    namespace_name: str | None = Field(default=const.LOCAL_NAMESPACE_NAME)
+    catalog_type: str | None = Field(default=const.LOCAL_CATALOG_TYPE)
     dataset_dir: str | Path | None = Field(default=None)
     cache_dir: str | Path | None = Field(default=None)
     scripts_dir: str | Path | None = Field(default=None)
@@ -79,7 +47,7 @@ class LocalCatalog(PydanticBaseModel):
         """Assign dataset, cache, and scripts dirs based on warehouse dir."""
         if "warehouse_dir" in values and values["warehouse_dir"] is not None:
             values['dataset_dir'] = \
-                Path(values["warehouse_dir"]) / const.DATASET_DIR
+                Path(values["warehouse_dir"]) / const.LOCAL_NAMESPACE_NAME
             values['cache_dir'] = \
                 Path(values["warehouse_dir"]) / const.CACHE_DIR
             values['scripts_dir'] = \
@@ -91,9 +59,9 @@ class RemoteCatalog(PydanticBaseModel):
     """Base model for remote catalog configuration."""
 
     warehouse_dir: str | Path | None = Field(default=const.REMOTE_WAREHOUSE_S3_PATH)
-    catalog_name: str | None = Field(default="iceberg")
-    namespace_name: str | None = Field(default="teehr")
-    catalog_type: str | None = Field(default="rest")
+    catalog_name: str | None = Field(default=const.REMOTE_CATALOG_NAME)
+    namespace_name: str | None = Field(default=const.REMOTE_NAMESPACE_NAME)
+    catalog_type: str | None = Field(default=const.REMOTE_CATALOG_TYPE)
     catalog_uri: str | None = Field(default=const.REMOTE_CATALOG_REST_URI)
 
     model_config = ConfigDict(
