5c8dc62229544b24225a2b4553f0dbba76d6aff9
samlamont <sam.lamont@gmail.com>
2025-11-04 10:52:32 -0500

    Timeseries table doc strings

--- src/teehr/evaluation/tables/timeseries_table.py
+++ src/teehr/evaluation/tables/timeseries_table.py
@@ -41,9 +41,7 @@ class TimeseriesTable(Table):
         write_mode: TableWriteEnum = "append",
         parallel: bool = False,
         max_workers: Union[int, None] = MAX_CPUS,
-        persist_dataframe: bool = False,
         drop_duplicates: bool = True,
-        location_id_field: str = "location_id",
         **kwargs
     ):
         """Import timeseries parquet data.
@@ -53,6 +51,18 @@ class TimeseriesTable(Table):
         in_path : Union[Path, str]
             Path to the timeseries data (file or directory) in
             parquet file format.
+        namespace_name : str, optional
+            The namespace name to write to, by default None, which means the
+            namespace_name of the active catalog is used.
+        catalog_name : str, optional
+            The catalog name to write to, by default None, which means the
+            catalog_name of the active catalog is used.
+        extraction_function : callable, optional
+            A function to extract and transform the data from the input files
+            to the TEEHR data model.
+        pattern : str, optional
+            The glob pattern to use when searching for files in a directory.
+            Default is '**/*.parquet' to search for all parquet files recursively.
         field_mapping : dict, optional
             A dictionary mapping input fields to output fields.
             Format: {input_field: output_field}
@@ -67,13 +77,13 @@ class TimeseriesTable(Table):
             ("nwm12, "nwm21", "nwm22", or "nwm30"), respectively.
         write_mode : TableWriteEnum, optional (default: "append")
             The write mode for the table.
-            Options are "append", "upsert", and "overwrite".
-            If "append", the table will be appended with new data that does
-            already exist.
+            Options are "append", "upsert", and "create_or_replace".
+            If "append", the table will be appended without checking
+            existing data.
             If "upsert", existing data will be replaced and new data that
             does not exist will be appended.
-            If "overwrite", existing partitions receiving new data are
-            overwritten.
+            If "create_or_replace", a new table will be created or an existing
+            table will be replaced.
         parallel : bool, optional (default: False)
             Whether to process files in parallel. Default is False.
         max_workers : Union[int, None], optional
@@ -82,12 +92,8 @@ class TimeseriesTable(Table):
             ProcessPoolExecutor. If in_path is a file, this parameter is ignored.
             The default value is max(os.cpu_count() - 1, 1).
             If None, os.process_cpu_count() is used.
-        persist_dataframe : bool, optional (default: False)
-            Whether to repartition and persist the pyspark dataframe after
-            reading from the cache. This can improve performance when loading
-            a large number of files from the cache.
         drop_duplicates : bool, optional (default: True)
-            Whether to drop duplicates from the dataframe.
+            Whether to drop duplicates from the DataFrame during validation.
         **kwargs
             Additional keyword arguments are passed to pd.read_parquet().
 
@@ -128,9 +134,8 @@ class TimeseriesTable(Table):
             write_mode=write_mode,
             parallel=parallel,
             max_workers=max_workers,
-            persist_dataframe=persist_dataframe,
             drop_duplicates=drop_duplicates,
-            primary_location_id_field=location_id_field,
+            primary_location_id_field="location_id",
             **kwargs
         )
         self._load_table()
@@ -160,6 +165,15 @@ class TimeseriesTable(Table):
         in_path : Union[Path, str]
             Path to the timeseries data (file or directory) in
             csv file format.
+        namespace_name : str, optional
+            The namespace name to write to, by default None, which means the
+            namespace_name of the active catalog is used.
+        catalog_name : str, optional
+            The catalog name to write to, by default None, which means the
+            catalog_name of the active catalog is used.
+        extraction_function : callable, optional
+            A function to extract and transform the data from the input files
+            to the TEEHR data model.
         pattern : str, optional (default: "**/*.csv")
             The pattern to match files. Controls which files are loaded from
             the directory. If in_path is a file, this parameter is ignored.
@@ -177,12 +191,13 @@ class TimeseriesTable(Table):
             ("nwm12, "nwm21", "nwm22", or "nwm30"), respectively.
         write_mode : TableWriteEnum, optional (default: "append")
             The write mode for the table.
-            Options are "append", "upsert", and "overwrite".
-            If "append", the table will be appended with new data that does
-            already exist.
+            Options are "append", "upsert", and "create_or_replace".
+            If "append", the table will be appended without checking
+            existing data.
             If "upsert", existing data will be replaced and new data that
             does not exist will be appended.
-            If "overwrite", existing partitions receiving new data are overwritten
+            If "create_or_replace", a new table will be created or an existing
+            table will be replaced.
         parallel : bool, optional (default: False)
             Whether to process files in parallel. Default is False.
         max_workers : Union[int, None], optional
@@ -191,12 +206,8 @@ class TimeseriesTable(Table):
             ProcessPoolExecutor. If in_path is a file, this parameter is ignored.
             The default value is max(os.cpu_count() - 1, 1).
             If None, os.process_cpu_count() is used.
-        persist_dataframe : bool, optional (default: False)
-            Whether to repartition and persist the pyspark dataframe after
-            reading from the cache. This can improve performance when loading
-            a large number of files from the cache.
         drop_duplicates : bool, optional (default: True)
-            Whether to drop duplicates from the dataframe.
+            Whether to drop duplicates from the DataFrame during validation.
         **kwargs
             Additional keyword arguments are passed to pd.read_csv().
 
@@ -237,9 +248,8 @@ class TimeseriesTable(Table):
             write_mode=write_mode,
             parallel=parallel,
             max_workers=max_workers,
-            persist_dataframe=persist_dataframe,
             drop_duplicates=drop_duplicates,
-            primary_location_id_field=location_id_field,
+            primary_location_id_field="location_id",
             **kwargs
         )
         self._load_table()
@@ -257,9 +267,7 @@ class TimeseriesTable(Table):
         write_mode: TableWriteEnum = "append",
         parallel: bool = False,
         max_workers: Union[int, None] = MAX_CPUS,
-        persist_dataframe: bool = False,
         drop_duplicates: bool = True,
-        location_id_field: str = "location_id",
         **kwargs
     ):
         """Import timeseries netcdf data.
@@ -269,6 +277,15 @@ class TimeseriesTable(Table):
         in_path : Union[Path, str]
             Path to the timeseries data (file or directory) in
             netcdf file format.
+        namespace_name : str, optional
+            The namespace name to write to, by default None, which means the
+            namespace_name of the active catalog is used.
+        catalog_name : str, optional
+            The catalog name to write to, by default None, which means the
+            catalog_name of the active catalog is used.
+        extraction_function : callable, optional
+            A function to extract and transform the data from the input files
+            to the TEEHR data model.
         pattern : str, optional (default: "**/*.nc")
             The pattern to match files. Controls which files are loaded from
             the directory. If in_path is a file, this parameter is ignored.
@@ -286,12 +303,13 @@ class TimeseriesTable(Table):
             ("nwm12, "nwm21", "nwm22", or "nwm30"), respectively.
         write_mode : TableWriteEnum, optional (default: "append")
             The write mode for the table.
-            Options are "append", "upsert", and "overwrite".
-            If "append", the table will be appended with new data that does
-            already exist.
+            Options are "append", "upsert", and "create_or_replace".
+            If "append", the table will be appended without checking
+            existing data.
             If "upsert", existing data will be replaced and new data that
             does not exist will be appended.
-            If "overwrite", existing partitions receiving new data are overwritten
+            If "create_or_replace", a new table will be created or an existing
+            table will be replaced.
         parallel : bool, optional (default: False)
             Whether to process files in parallel. Default is False.
         max_workers : Union[int, None], optional
@@ -305,7 +323,7 @@ class TimeseriesTable(Table):
             reading from the cache. This can improve performance when loading
             a large number of files from the cache.
         drop_duplicates : bool, optional (default: True)
-            Whether to drop duplicates from the dataframe.
+            Whether to drop duplicates from the DataFrame during validation.
         **kwargs
             Additional keyword arguments are passed to xr.open_dataset().
 
@@ -346,9 +364,8 @@ class TimeseriesTable(Table):
             write_mode=write_mode,
             parallel=parallel,
             max_workers=max_workers,
-            persist_dataframe=persist_dataframe,
             drop_duplicates=drop_duplicates,
-            primary_location_id_field=location_id_field,
+            primary_location_id_field="location_id",
             **kwargs
         )
         self._load_table()
@@ -374,9 +391,7 @@ class TimeseriesTable(Table):
         write_mode: TableWriteEnum = "append",
         parallel: bool = False,
         max_workers: Union[int, None] = MAX_CPUS,
-        persist_dataframe: bool = False,
         drop_duplicates: bool = True,
-        location_id_field: str = "location_id",
         **kwargs
     ):
         """Import timeseries from FEWS PI-XML data format.
@@ -386,6 +401,15 @@ class TimeseriesTable(Table):
         in_path : Union[Path, str]
             Path to the timeseries data (file or directory) in
             xml file format.
+        namespace_name : str, optional
+            The namespace name to write to, by default None, which means the
+            namespace_name of the active catalog is used.
+        catalog_name : str, optional
+            The catalog name to write to, by default None, which means the
+            catalog_name of the active catalog is used.
+        extraction_function : callable, optional
+            A function to extract and transform the data from the input files
+            to the TEEHR data model.
         pattern : str, optional (default: "**/*.xml")
             The pattern to match files. Controls which files are loaded from
             the directory. If in_path is a file, this parameter is ignored.
@@ -417,12 +441,13 @@ class TimeseriesTable(Table):
             ("nwm12, "nwm21", "nwm22", or "nwm30"), respectively.
         write_mode : TableWriteEnum, optional (default: "append")
             The write mode for the table.
-            Options are "append", "upsert", and "overwrite".
-            If "append", the table will be appended with new data that does
-            already exist.
+            Options are "append", "upsert", and "create_or_replace".
+            If "append", the table will be appended without checking
+            existing data.
             If "upsert", existing data will be replaced and new data that
             does not exist will be appended.
-            If "overwrite", existing partitions receiving new data are overwritten
+            If "create_or_replace", a new table will be created or an existing
+            table will be replaced.
         parallel : bool, optional (default: False)
             Whether to process files in parallel. Default is False.
         max_workers : Union[int, None], optional
@@ -431,10 +456,6 @@ class TimeseriesTable(Table):
             ProcessPoolExecutor. If in_path is a file, this parameter is ignored.
             The default value is max(os.cpu_count() - 1, 1).
             If None, os.process_cpu_count() is used.
-        persist_dataframe : bool, optional (default: False)
-            Whether to repartition and persist the pyspark dataframe after
-            reading from the cache. This can improve performance when loading
-            a large number of files from the cache.
         drop_duplicates : bool, optional (default: True)
             Whether to drop duplicates from the dataframe.
 
@@ -483,9 +504,8 @@ class TimeseriesTable(Table):
             write_mode=write_mode,
             parallel=parallel,
             max_workers=max_workers,
-            persist_dataframe=persist_dataframe,
             drop_duplicates=drop_duplicates,
-            primary_location_id_field=location_id_field,
+            primary_location_id_field="location_id",
             **kwargs
         )
         self._load_table()
@@ -499,9 +519,7 @@ class TimeseriesTable(Table):
         constant_field_values: dict = None,
         location_id_prefix: str = None,
         write_mode: TableWriteEnum = "append",
-        persist_dataframe: bool = False,
         drop_duplicates: bool = True,
-        location_id_field: str = "location_id",
     ):
         """Load data from an in-memory dataframe.
 
@@ -509,6 +527,12 @@ class TimeseriesTable(Table):
         ----------
         df : Union[pd.DataFrame, ps.DataFrame]
             Pandas or PySparkDataFrame to load into the table.
+        namespace_name : str, optional
+            The namespace name to write to, by default None, which means the
+            namespace_name of the active catalog is used.
+        catalog_name : str, optional
+            The catalog name to write to, by default None, which means the
+            catalog_name of the active catalog is used.
         field_mapping : dict, optional
             A dictionary mapping input fields to output fields.
             Format: {input_field: output_field}
@@ -523,16 +547,13 @@ class TimeseriesTable(Table):
             ("nwm12, "nwm21", "nwm22", or "nwm30"), respectively.
         write_mode : TableWriteEnum, optional (default: "append")
             The write mode for the table.
-            Options are "append", "upsert", and "overwrite".
-            If "append", the table will be appended with new data that does
-            already exist.
+            Options are "append", "upsert", and "create_or_replace".
+            If "append", the table will be appended without checking
+            existing data.
             If "upsert", existing data will be replaced and new data that
             does not exist will be appended.
-            If "overwrite", existing partitions receiving new data are overwritten.
-        persist_dataframe : bool, optional (default: False)
-            Whether to repartition and persist the pyspark dataframe after
-            reading from the cache. This can improve performance when loading
-            a large number of files from the cache.
+            If "create_or_replace", a new table will be created or an existing
+            table will be replaced.
         drop_duplicates : bool, optional (default: True)
             Whether to drop duplicates from the dataframe.
         """ # noqa
@@ -551,9 +572,8 @@ class TimeseriesTable(Table):
             field_mapping=field_mapping,
             constant_field_values=constant_field_values,
             primary_location_id_prefix=location_id_prefix,
-            primary_location_id_field=location_id_field,
+            primary_location_id_field="location_id",
             write_mode=write_mode,
-            persist_dataframe=persist_dataframe,
             drop_duplicates=drop_duplicates
         )
         self._load_table()
