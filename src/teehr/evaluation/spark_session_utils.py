517373f745818d288c426ea2183d2c34676b5afa
samlamont <sam.lamont@gmail.com>
2025-11-06 13:38:56 -0500

    fix boto cred check

--- src/teehr/evaluation/spark_session_utils.py
+++ src/teehr/evaluation/spark_session_utils.py
@@ -128,14 +128,15 @@ def create_spark_session(
     """
     logger.info(f"🚀 Creating Spark session: {app_name}")
 
-    # Use boto3 to check for AWS credentials if not provided
-    # and set them in environment variables
+    # Use botocore to check for AWS credentials
+    # and set them as environment variables
     session = botocore.session.Session()
     credentials = session.get_credentials()
-    aws_access_key = credentials.access_key
-    aws_secret_key = credentials.secret_key
-    os.environ["AWS_ACCESS_KEY_ID"] = aws_access_key
-    os.environ["AWS_SECRET_ACCESS_KEY"] = aws_secret_key
+    if credentials is not None:
+        aws_access_key = credentials.access_key
+        aws_secret_key = credentials.secret_key
+        os.environ["AWS_ACCESS_KEY_ID"] = aws_access_key
+        os.environ["AWS_SECRET_ACCESS_KEY"] = aws_secret_key
     os.environ["AWS_REGION"] = const.AWS_REGION
 
     # Get the base configuration with common settings
