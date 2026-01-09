37278e8a7515d2017f63d96a63a14557b11994d8
samlamont <sam.lamont@gmail.com>
2026-01-09 15:50:19 -0500

    bump dev version

--- src/teehr/__init__.py
+++ src/teehr/__init__.py
@@ -1,7 +1,7 @@
 """Initialize the TEEHR package."""
 import warnings
 
-__version__ = "0.6.0dev5"
+__version__ = "0.6.0dev6"
 
 with warnings.catch_warnings():
     warnings.simplefilter("ignore", UserWarning)
