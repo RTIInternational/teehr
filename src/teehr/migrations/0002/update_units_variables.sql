b51b96405107c0c9eb3d44d910c3dad3c8f966f4
samlamont <sam.lamont@gmail.com>
2026-03-24 13:49:47 -0400

    add migration to update unit and variable tables

---
+++ src/teehr/migrations/0002/update_units_variables.sql
@@ -0,0 +1,8 @@
+INSERT INTO units VALUES
+    ("C", "Degrees Celsius"),
+    ("K", "Kelvin");
+
+INSERT INTO variables VALUES
+    ("temperature_hourly_mean", "Hourly Mean Temperature");
+
+DELETE FROM variables WHERE name = "streamflow";
