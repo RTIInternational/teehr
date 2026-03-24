b10738a438af74d3f71dc10007a87ea468d8aaa5
samlamont <sam.lamont@gmail.com>
2026-03-24 10:58:07 -0400

    add default units and variable name

--- src/teehr/migrations/0001/v0_6_0_template.sql
+++ src/teehr/migrations/0001/v0_6_0_template.sql
@@ -58,15 +58,17 @@ CREATE TABLE IF NOT EXISTS location_attributes(
     value STRING
 ) USING iceberg;
 
--- Should these be if not exists?
 INSERT INTO units VALUES
     ("m^3/s", "Cubic Meters Per Second"),
     ("ft^3/s", "Cubic Feet Per Second"),
     ("km^2", "Square Kilometers"),
-    ("mm/s", "Millimeters Per Second");
+    ("mm/s", "Millimeters Per Second"),
+    ("C", "Degrees Celsius"),
+    ("K", "Kelvin");
 
 INSERT INTO variables VALUES
     ("streamflow", "Hourly Instantaneous Streamflow"),
     ("streamflow_hourly_inst", "Hourly Instantaneous Streamflow"),
     ("streamflow_daily_mean", "Daily Mean Streamflow"),
-    ("rainfall_hourly_rate", "Hourly Rainfall Rate");
+    ("rainfall_hourly_rate", "Hourly Rainfall Rate"),
+    ("temperature_hourly_mean", "Hourly Mean Temperature");
