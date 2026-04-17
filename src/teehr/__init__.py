c9ef70be902f6c1a2c39330d48e95aea763273c6
Sam Lamont <samlamont@users.noreply.github.com>
2026-04-17 14:27:26 -0400

    750 release 062 (#751)
    * bump version; convert changelog to markdown
    
    * update changelog

--- src/teehr/__init__.py
+++ src/teehr/__init__.py
@@ -1,7 +1,7 @@
 """Initialize the TEEHR package."""
 import warnings
 
-__version__ = "0.6.1"
+__version__ = "0.6.2"
 
 with warnings.catch_warnings():
     warnings.simplefilter("ignore", UserWarning)
