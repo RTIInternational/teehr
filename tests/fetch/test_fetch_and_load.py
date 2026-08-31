272992d2be539634f980652b955fcfdb4060542e
samlamont <sam.lamont@gmail.com>
2026-08-31 14:03:14 -0400

    refactor: create nwm point fetching plan; fix test

--- tests/fetch/test_fetch_and_load.py
+++ tests/fetch/test_fetch_and_load.py
@@ -184,9 +184,10 @@ def test_fetch_and_load_nwm_operational_points(function_scope_evaluation_templat
     assert np.isclose(updated_df.value.sum(), np.float32(492485.03))
 
 
-def test_fetch_and_load_nwm_operational_grids(tmpdir):
+@pytest.mark.function_scope_evaluation_template
+def test_fetch_and_load_nwm_operational_grids(function_scope_evaluation_template):
     """Test the NWM forecast grids fetch and load."""
-    ev = LocalReadWriteEvaluation(dir_path=tmpdir, create_dir=True)
+    ev = function_scope_evaluation_template
 
     ev.locations.load_spatial(in_path=ZONAL_LOCATIONS)
 
